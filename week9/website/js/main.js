0420ffb98c98a1c58991e5b563387014ca1d126d
lukemich47 <lukemich47@gmail.com>
2016-04-07 09:50:58 -0400

    changes

--- week9/website/js/main.js
+++ week9/website/js/main.js
@@ -1,3 +1,7 @@
 function helloworld() {
     document.getElementById('foo').innerHTML = 'Hello JavaScript!';
 }
+// displayDate function
+function displayDate() {
+    document.getElementById("foo").innerHTML = Date();
+}
